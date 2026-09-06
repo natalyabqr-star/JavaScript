ee666af45c4a36125ac2d52cdb4f4faf102c0240
natalyabqr-star <natalyabqr@gmail.com>
2026-09-06 00:12:39 -0300

    Atividade JS

---
+++ ex009.js
@@ -0,0 +1,8 @@
+let idade = 18
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('Você é menor de idade e não pode votar!')
+}
+else if (idade < 18) {
+    console.log('Você é maior de idade e pode votar, mas não é obrigatório!')
+}
