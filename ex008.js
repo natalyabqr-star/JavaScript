a4ca844164a854f140cc67f3b9d70f31fa03ed49
natalyabqr-star <natalyabqr@gmail.com>
2026-09-09 17:39:17 -0300

    Organiza exercícios de JavaScript em pasta

--- ex008.js
+++
@@ -1,6 +0,0 @@
-let vel = 100
-console.log(`A velocidade do seu carro é ${vel} km/h`)
-if (vel > 60) { //condição simples
-    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
-}
-console.log('Dirija sempre usando cinto de segurança!')
--- ex009.js
+++
@@ -1,8 +0,0 @@
-let idade = 18
-console.log(`Você tem ${idade} anos.`)
-if (idade < 16) {
-    console.log('Você é menor de idade e não pode votar!')
-}
-else if (idade < 18) {
-    console.log('Você é maior de idade e pode votar, mas não é obrigatório!')
-}
