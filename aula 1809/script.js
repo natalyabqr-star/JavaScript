2852ef7620fd7ea3fd012c19f2b52d8421524d74
natalyabqr-star <natalyabqr@gmail.com>
2026-09-18 20:19:13 -0300

    Adicionando as funções

--- aula 1809/script.js
+++ aula 1809/script.js
@@ -13,6 +13,8 @@ const boolean = true // pode ser true ou false
 function salvarExibirNomeIdade () {
     let nome = prompt('Olá, qual é seu nome') // o let serve para guardar e se preciso podemos manipular
     let idade = prompt('Oi' + nome + ', agora, qual é a sua idade?')
+
+    document.body.innerHTML = 'Seu nome é' + nome + "e voce tem" + idade
 }
 salvarExibirNomeIdade()
 
