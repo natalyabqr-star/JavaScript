d5b89d2fe3eec374a93829c46cfc0ab89982f886
natalyabqr-star <natalyabqr@gmail.com>
2026-09-18 20:10:03 -0300

    Adicionando funções

---
+++ aula 1809/script.js
@@ -0,0 +1,23 @@
+alert("Olá mundo")
+
+//sintaxe
+
+
+//tipos 
+const string = 'Texto'
+const number =  10  //podeia entrar numeros diversos
+const boolean = true // pode ser true ou false
+
+
+//funções
+function salvarExibirNomeIdade () {
+    let nome = prompt('Olá, qual é seu nome') // o let serve para guardar e se preciso podemos manipular
+    let idade = prompt('Oi' + nome + ', agora, qual é a sua idade?')
+}
+salvarExibirNomeIdade()
+
+// estrutura de controle 
+
+// arrays
+
+// objetos 
