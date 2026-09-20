9677703c362f752c6d695728c810a54775d574f0
natalyabqr-star <natalyabqr@gmail.com>
2026-09-20 17:36:09 -0300

    JavaScript foi colocado

--- Verificador idade/scrpit.js
+++ Verificador idade/scrpit.js
@@ -1,13 +1,11 @@
-function carregar() {
-let msg = window.document.getElementById('msg')
-let img = window.document.getElementById('imagem')
-let data= new Date()
-let hora = data.getHours
-msg.innerHTML = `Agora são ${horas} horas.`
-}
-if (hora >=0 && hora < 12) {
-    // BOM DIA 
-}
-else if (hora >= 12 && hora < 18) {
-    //BOA TARDE
+function verificar (){
+    let data = new Date()
+    let ano = data.getFullYear //é a função que tras o ano atual 
+    let fano = document.getElementById('txtano')
+    let res = document.querySelector('div#res')
+    if(fano.value.length == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente')
+    } else {
+        window.alert('Tudo ok')
+    }
 }
