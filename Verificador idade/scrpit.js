c57965bc0c7df07ed341d7f43e74f24f0ea118c3
natalyabqr-star <natalyabqr@gmail.com>
2026-09-20 17:42:34 -0300

    Feita algumas modificações do no HTML para conseguir se ligar ao JS

--- Verificador idade/scrpit.js
+++ Verificador idade/scrpit.js
@@ -1,6 +1,6 @@
 function verificar (){
     let data = new Date()
-    let ano = data.getFullYear //é a função que tras o ano atual 
+    let ano = data.getFullYear ()//é a função que tras o ano atual 
     let fano = document.getElementById('txtano')
     let res = document.querySelector('div#res')
     if(fano.value.length == 0 || fano.value > ano) {
