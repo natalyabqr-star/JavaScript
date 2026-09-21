bf0eefd8e0589f18536cfb08ec11b3beb0fa8400
natalyabqr-star <natalyabqr@gmail.com>
2026-09-21 19:04:56 -0300

    JavaScript completo, verificador de idade completos

--- Verificador idade/scrpit.js
+++ Verificador idade/scrpit.js
@@ -1,11 +1,23 @@
-function verificar (){
+function verificar() {
     let data = new Date()
-    let ano = data.getFullYear ()//é a função que tras o ano atual 
+    let ano = data.getFullYear() // Ano atual
     let fano = document.getElementById('txtano')
     let res = document.querySelector('div#res')
-    if(fano.value.length == 0 || fano.value > ano) {
+    
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('Verifique os dados e tente novamente')
     } else {
-        window.alert('Tudo ok')
-    }
+        let fsex = document.getElementsByName('radsex') // Adicionadas as aspas
+        let idade = ano - Number(fano.value)
+        let genero = ''
+        
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else if (fsex[1].checked) { // Adicionado o .checked
+            genero = 'Mulher'
+        }
+        
+        // Exemplo de como finalizar o innerHTML para exibir o resultado
+        res.innerHTML = `Detectamos ${genero} com incríveis ${idade} anos.`
+    } 
 }
